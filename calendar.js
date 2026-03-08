6943e0e40c883ffd5500faf8dd3ab3756a3db1db
Techin747 <163860715+Techin747@users.noreply.github.com>
2026-03-08 17:14:50 +0700

    Add functions to create and list calendar events

---
+++ calendar.js
@@ -0,0 +1,14 @@
+export async function createEvent(title,date){
+
+ console.log("create calendar event",title,date)
+
+}
+
+export async function listEvents(){
+
+ return [
+  "09:00 สอบคณิต",
+  "13:00 ประชุมทีม"
+ ]
+
+}
